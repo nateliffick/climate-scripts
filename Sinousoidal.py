a7c0530f0a94bf9674f4711302453ea331c1baba
nateliffick <65188063+nateliffick@users.noreply.github.com>
2020-06-08 19:13:54 +0100

    Update Sinousoidal.py

--- Sinousoidal.py
+++ Sinousoidal.py
@@ -4,12 +4,19 @@ Created on Wed Dec  4 11:53:46 2019
 
 @author: natel
 """
-
+# assigning the plot limitations
 ig,ax=plt.subplots(1,1)
+# assigning the observed values as the color black and label it yearly temp
 ax.plot(doy_clim["obs"],color="black", label = "Yearly Temp")
 
+# assigning the observed values as the color yellow and label it sin curve
+# this is the basic sin curve that follow natural law
 ax.plot(doy_clim["clim"],color="yellow",alpha= 7, label = "Sin curve")
+# adding a legend
 ax.legend()
+# the x axis is going to auto format to the current dates
 fig.autofmt_xdate()
+# the x axis will be labeled by the dates 2016-2019
 ax.set_xlabel("2016-2019 dates")
+# the y axis is going to be labeled for the temperature in C
 ax.set_ylabel("Temperature (deg C)")
