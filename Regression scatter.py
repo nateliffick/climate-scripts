180eadcf306abee02475bd037ace72dab57bf98b
nateliffick <65188063+nateliffick@users.noreply.github.com>
2020-06-08 19:34:53 +0100

    Update Regression scatter.py

--- Regression scatter.py
+++ Regression scatter.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-This is my analysis script. I can copy/paste all relevant code from
-the jupyter notebooks in to this file. When I am ready, I will run it with 
-the big green 'play' button at the top of the screeen. I can then inspect 
-variables in the "Variable explorer" window (top right); and I can see the output
-from my code (e.g. plots) in the IPython console (bottom right).
+
 
 """
 # Import modules
@@ -12,26 +8,39 @@ import pandas as pd, numpy as np
 import matplotlib.pyplot as plt
 import tom
 
-fin="C:/Users/natel/OneDrive/Desktop/Hydro_monitoring/python_code/Campus_Met_Data_Student.csv"
+# assigns a variable to the csv file
+fin="Campus_Met_Data_Student.csv"
+# strips the date and time from the csv file
 date_parser=lambda x: pd.datetime.strptime(x,"%d/%m/%Y %H:%M")
 #obs=pd.read_csv(fin,parse_dates=["TIMESTAMP"],date_parser=date_parser,index_col=0)
 obs=pd.read_csv(fin,parse_dates=True,index_col=0)
+# resample the means 
 obs=obs.resample("D").mean()
+# prints all the column heading in the csv
 obs.head(50)
 
+# states all the variables in the csv and formates the specific columns subjected to autoregression
 pred,r2 = tom.auto_regress(obs["AirTC_Avg"],obs[["AirTC_Avg","RH"]],2019,1,0.05,True)
 
+# prints the pred variable which is included in the csv file
 pred
 
+# creates a plot
 fig,ax=plt.subplots(1,1)
+# creates a scatter plot for observed values and simulated values
 ax.scatter(pred["obs"],pred["sim"],s=1)
+# sets the scatter plots x and y axis limits
 ax.set_xlim([-5,26])
 ax.set_ylim([-5,26])
+# sets the scatter plot x and y labels
 ax.set_xlabel("Observed Air Temperature (deg C)")
 ax.set_ylabel("Modeled Air Temperature (deg C)")
+# assigns grid lines to the x and y axis
 ax.yaxis.grid()
 ax.xaxis.grid()
+# arranges the scatter plot points between the values -5 and 27
 x=np.arange(-5,27)
+# assigns the scatter plot points as black 
 ax.plot(x,x,color='black')
 
 
